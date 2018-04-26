dde1b340479bf0a263d0cf5629758928ef20d6a3
lohnns <lohnns.dev@gmail.com>
2018-04-25 20:03:45 -0400

    init hangman project

---
+++ hangman/src/components/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Hangman game</h1>
+        </header>
+        <p className="App-intro">
+          Here will come the game...
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
