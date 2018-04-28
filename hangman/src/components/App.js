98d22b29a0b9454dbf9e73d57c90ae0e26e207d8
lohnns <lohnns.dev@gmail.com>
2018-04-28 19:24:16 -0400

    implementing win and start new game

--- hangman/src/components/App.js
+++ hangman/src/components/App.js
@@ -6,22 +6,34 @@ class App extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {
-            answer: 'Integration'.toUpperCase(),
-            usedLetters: new Set()
-        }
+        this.state = this.initGame();
+    }
+
+    initGame = () => {
+        const answer = 'Abc'.toUpperCase();
+        return {
+            answer: answer,
+            usedLetters: new Set(),
+            lettersToFound: new Set([...answer])
+        };
     }
 
     guessLetter = letter => {
-        const { usedLetters } = this.state;
+        const {usedLetters, lettersToFound} = this.state;
         usedLetters.add(letter);
+        lettersToFound.delete(letter);
         this.setState({
-            usedLetters: new Set(usedLetters)
+            usedLetters: new Set(usedLetters),
+            lettersToFound: new Set(lettersToFound)
         });
     }
 
+    startNewGame = () => {
+        this.setState(this.initGame());
+    }
+
     render() {
-        const { answer, usedLetters } = this.state;
+        const {answer, lettersToFound, usedLetters} = this.state;
         return (
             <div className="App">
                 <header className="App-header">
@@ -29,9 +41,17 @@ class App extends Component {
                 </header>
                 <p className="App-intro">
                     <div id='answer' className='hg-sentence'>{
-                        answer.split('').map(char => (usedLetters.has(char) ? char : '_') + ' ')
+                        [...answer].map(char => usedLetters.has(char) ? char : '_')
                     }</div>
-                    <LettersPanel guessLetter={this.guessLetter}/>
+                    {
+                        lettersToFound.size !== 0 ?
+                            <LettersPanel guessLetter={this.guessLetter}/>
+                            :
+                            <div>
+                                <div>You win in {usedLetters.size} guesses!</div>
+                                <input type="button" value="Play again?" onClick={this.startNewGame}/>
+                            </div>
+                    }
                 </p>
             </div>
         );
